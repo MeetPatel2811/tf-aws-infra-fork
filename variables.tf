e5843fc006c0602b4233c71c34b8c011cb8e3d18
MeetPatel2811 <patel.meetal@northeastern.edu>
2025-02-19 20:17:54 -0500

    Update the variable file

--- variables.tf
+++ variables.tf
@@ -1,37 +1,53 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources into"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "aws_profile" {
   description = "AWS CLI profile to use (e.g., dev or demo)"
   type        = string
   default     = "dev"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "region" {
+  description = "AWS region where all VPCs will be created"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "us-east-1"
 }
 
-variable "vpc_name" {
-  description = "Name tag for the VPC"
-  type        = string
-  default     = "CSYE-VPC"
+variable "vpc_names" {
+  description = "Map of VPC names; key 'main' must be defined for resources referencing aws_vpc.main"
+  type        = map(string)
+  default = {
+    main = "CSYE-VPC1"
+    vpc2 = "CSYE-VPC2"
+    vpc3 = "CSYE-VPC3"
+  }
+}
+
+variable "vpc_cidrs" {
+  description = "Map of CIDR blocks for the VPCs"
+  type        = map(string)
+  default = {
+    main = "10.0.0.0/16"
+    vpc2 = "10.1.0.0/16"
+    vpc3 = "10.2.0.0/16"
+  }
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDRs for public subnets (one per AZ)"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "Map of lists of CIDRs for public subnets for each VPC"
+  type        = map(list(string))
+  default = {
+    main = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    vpc2 = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+    vpc3 = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
+  }
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of CIDRs for private subnets (one per AZ)"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  description = "Map of lists of CIDRs for private subnets for each VPC"
+  type        = map(list(string))
+  default = {
+    main = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    vpc2 = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+    vpc3 = ["10.2.101.0/24", "10.2.102.0/24", "10.2.103.0/24"]
+  }
 }
 
 variable "internet_gateway_name" {
@@ -59,7 +75,7 @@ variable "public_route_dest_cidr" {
 }
 
 variable "availability_zones" {
-  description = "List of availability zones for the subnets"
+  description = "List of availability zones to use for the subnets"
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
