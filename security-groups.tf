82060fbd3970ee7a4af2b321ef90d4970ee54b0a
MeetPatel2811 <patel.meetal@northeastern.edu>
2025-02-25 20:15:47 -0500

    Add the security groups file

---
+++ security-groups.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "app_sg" {
+  name        = "${var.name_prefix}-app-sg"
+  description = "Security group for EC2 instances hosting web applications"
+  vpc_id      = aws_vpc.vpc["main"].id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Application Port"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-app-sg"
+  }
+}
+
