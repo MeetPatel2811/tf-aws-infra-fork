3a62bff543f78e3dd90393e4b511dbf17294bfc4
MeetPatel2811 <patel.meetal@northeastern.edu>
2025-04-03 11:20:19 -0400

    Add the load balancer code

---
+++ load_balancer.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "web_alb" {
+  name               = "${var.name_prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
+  tags = {
+    Name = "${var.name_prefix}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "${var.name_prefix}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc["main"].id
+
+  health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = var.healthy_threshold
+    unhealthy_threshold = var.unhealthy_threshold
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web_asg.name
+  lb_target_group_arn    = aws_lb_target_group.web_target_group.arn
+}
