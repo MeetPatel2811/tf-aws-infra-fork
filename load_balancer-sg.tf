45031df01aa205582dfd9f76d0a0505e2c7dcf63
MeetPatel2811 <patel.meetal@northeastern.edu>
2025-04-03 11:12:41 -0400

    Add load balancer security group

---
+++ load_balancer-sg.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "${var.name_prefix}-lb-sg"
+  description = "Security group for the Load Balancer"
+  vpc_id      = aws_vpc.vpc["main"].id
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-lb-sg"
+  }
+}
